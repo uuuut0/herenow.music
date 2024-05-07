167daf952cbea8dac97234d8cef11bebdc814714
uuuut0 <u42t00@gmail.com>
2024-05-07 09:41:08 +0800

    artistpage: lightbox script added

--- script/about.js
+++ script/about.js
@@ -1,88 +1,117 @@
-function mobilemenuToggle(){
-    const mobileMenu =document.querySelector('.header-navLinks-wrapper-mobile')
+function mobilemenuToggle() {
+    const mobileMenu = document.querySelector('.header-navLinks-wrapper-mobile')
 
     // alert(mobileMenu.style.display)
     // mobileMenu.classList.add('header-nav-mobile-active')
     // mobileMenu.style.height= mobileMenu.style.height ==='0px'?'500px':'0px'
-        mobileMenu.style.display= mobileMenu.style.display ==='none'?'block':'none'
-        // mobileMenu.style.top= mobileMenu.style.top ==='-10px'?'60px':'-10px'
-        // if(mobileMenu.style.display=='none'){
-        //     // mobileMenu.classList.add('header-nav-mobile-active')
-        //     alert('y')
-        // }else{
-        //     // mobileMenu.classList.remove('header-nav-mobile-active')
-        //     alert('n')
+    mobileMenu.style.display = mobileMenu.style.display === 'none' ? 'block' : 'none'
+    // mobileMenu.style.top= mobileMenu.style.top ==='-10px'?'60px':'-10px'
+    // if(mobileMenu.style.display=='none'){
+    //     // mobileMenu.classList.add('header-nav-mobile-active')
+    //     alert('y')
+    // }else{
+    //     // mobileMenu.classList.remove('header-nav-mobile-active')
+    //     alert('n')
 
-        // }
+    // }
 
 
 
 }
 
-// function elementToggleControl(){
-//     const toggleHead= document.querySelector('.toggle-head')
-//     const toggleBody= document.querySelector('.toggle-body')
-//     const activeTag ='toggle-show'
-//     if(toggleHead !==null && toggleBody !==null ){
+function lightboxControl() {
 
-//         toggleBody.classList.contains(activeTag) ? toggleBody.classList.remove(activeTag) : toggleBody.classList.add(activeTag)
+    const activeTag = 'lb-active';
+    const noscrollTag = 'noscroll'
+
+    const lb = document.querySelector('.photowall-lightbox')
+    const otherElement = document.querySelector('.lightbox-container .panel-icon-box')
+    const webBody = document.querySelector('body')
+
+    if (lb) {
+        lb.classList.contains(activeTag) ? lb.classList.remove(activeTag) : lb.classList.add(activeTag)
+    }
+    if (otherElement) {
+        // if(otherElement.style.display ==='none'){
+        //     otherElement.style.display='block'
+        // }else{
+        //     otherElement.style.display='none'
+        // }
+        otherElement.style.display === 'block' ? otherElement.style.display = 'none' : otherElement.style.display = 'block'
+
+        console.log(otherElement.style.display)
+    }
+    if (webBody) {
+        console.log(webBody.classList.length)
+        webBody.classList.contains(noscrollTag)? webBody.classList.remove(noscrollTag) :webBody.classList.add(noscrollTag)
+    }
+}
 
 
-//     }else{
-//         alert('failed to find toggle')
-//     }
-// }
+function elementListToggleControl(toggleElement, toggleIcon) {
+    const activeTag = 'toggle-show'
+    if (toggleIcon) { const activeIcon = '' }
 
 
-
-function elementListToggleControl(toggleElement,toggleIcon){
-    const activeTag ='toggle-show'
-    if(toggleIcon){const activeIcon =''}
-
-
-    const toggleBody= toggleElement.querySelector('.toggle-body')
+    const toggleBody = toggleElement.querySelector('.toggle-body')
 
     const prevActiveElement = toggleElement.parentNode.getElementsByClassName(activeTag)[0]
-    if(toggleBody !==null ){
+    if (toggleBody !== null) {
 
-        toggleBody.classList.contains(activeTag)?toggleBody.classList.remove(activeTag):toggleBody.classList.add(activeTag)
-        if(prevActiveElement ){
+        toggleBody.classList.contains(activeTag) ? toggleBody.classList.remove(activeTag) : toggleBody.classList.add(activeTag)
+        if (prevActiveElement) {
             prevActiveElement.classList.remove(activeTag)
         }
 
-        }else{
-        console.log('elementList','toggle from',toggleElement+'failed')
+    } else {
+        console.log('elementList', 'toggle from', toggleElement + 'failed')
     }
 
 
 }
 
-function init(){
-    const menubtn= document.querySelector('#menubtn')
-    const menubtn2= document.querySelector('#mobile-menu h1')
+function init() {
+    const menubtn = document.querySelector('#menubtn')
+    const menubtn2 = document.querySelector('#mobile-menu h1')
     // const aboutToggle = document.querySelector('.history-item')
     const aboutToggles = document.getElementsByClassName('toggle-element')
 
-    menubtn.addEventListener('click',()=>{
+    const lbBtn = document.querySelector('.photoitem')
+    const lbCloseBtn = document.querySelector('.lightbox-container .panel-icon-box')
 
-        mobilemenuToggle() 
+
+    //HEADER NAV MOBILE
+    menubtn.addEventListener('click', () => {
+
+        mobilemenuToggle()
 
     })
-    if(menubtn2!==null){
-        menubtn2.addEventListener('click',()=>{
+    if (menubtn2 !== null) {
+        menubtn2.addEventListener('click', () => {
 
             mobilemenuToggle()
         })
 
     }
 
-
-    for(let i=0;i<aboutToggles.length;i++){
-        aboutToggles[i].addEventListener('click',(e)=>{
-            elementListToggleControl(e.currentTarget)
-        })
+    // ABOUT TOGGLE
+    if (aboutToggles) {
+        for (let i = 0; i < aboutToggles.length; i++) {
+            aboutToggles[i].addEventListener('click', (e) => {
+                elementListToggleControl(e.currentTarget)
+            })
+        }
     }
 
 
+    //LIGHTBOX
+    if (lbBtn) {
+        lbBtn.addEventListener('click', lightboxControl)
+
+    }
+    if (lbCloseBtn) {
+        lbCloseBtn.addEventListener('click', lightboxControl)
+
+    }
 }
 init()
