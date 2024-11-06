9ef13f32d820c1b22b1cfde9917cc9e646b7acbc
l_n_u_t <80764869+uuuut0@users.noreply.github.com>
2024-11-06 15:34:34 +0800

    fix: route issue on gitpage deployed, on gotoblogpage.js url

--- script/gotoblogpage.js
+++ script/gotoblogpage.js
@@ -1,9 +1,11 @@
 function init(){
     const elements = document.querySelectorAll('.ofItem, .focus-block-wrap, .article-card, .news-card')
+    const urlbase = window.location.origin.includes("github.io")?'/herenow.music':''
     if(elements){
         elements.forEach((element)=>{
             element.addEventListener('click',()=>{
-                window.location.href = '/blog.html'
+        
+                window.location.href = urlbase+'blog.html'
                 console.log('click')
             })
             console.log(element)
